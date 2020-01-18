87d21193523f4d7b9353a055efee0425cddd4e83
novem2020 <60027144+novem2020@users.noreply.github.com>
2020-01-18 16:49:17 +0800

    Add files via upload

---
+++ m01.py
@@ -0,0 +1,10 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+
+while True:
+    display.scroll('KO NO DIO DA')
+    display.show(Image.MUSIC_QUAVERS)
+    sleep(1000)
+    display.show(Image.HEART_SMALL)
+    sleep(1000)
